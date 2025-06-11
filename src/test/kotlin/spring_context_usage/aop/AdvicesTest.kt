c5df5baa42b6c6fd31a13a30e9499767d9ef9e33
Galacsh <galacsh.dev@gmail.com>
2025-06-11 15:28:11 +0900

    여러 Advice 들을 사용해보는 테스트

---
+++ src/test/kotlin/spring_context_usage/aop/AdvicesTest.kt
@@ -0,0 +1,123 @@
+package spring_context_usage.aop
+
+import org.aspectj.lang.annotation.*
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AdvicesTest {
+
+    @Test
+    fun `정상 실행되는 경우 afterThrowing 빼고 모두 호출됨`() {
+        // Given
+        val context = AnnotationConfigApplicationContext().apply {
+            register(SampleBean::class.java)
+            register(SampleAspect::class.java)
+            register(AnnotationAwareAspectJAutoProxyCreator::class.java)
+            refresh()
+        }
+
+        // When
+        context.getBean(SampleBean::class.java).sampleMethod()
+
+        // Then
+        val expectedLog = listOf(
+            "Before (Around)",
+            "Before",
+            "After Returning",
+            "After",
+            "After (Around)",
+        )
+        assertEquals(expectedLog, SampleAspect.log)
+    }
+
+    @Test
+    fun `예외가 발생하는 경우 afterThrowing 호출됨`() {
+        // Given
+        val context = AnnotationConfigApplicationContext().apply {
+            register(SampleBean::class.java)
+            register(SampleAspect::class.java)
+            register(AnnotationAwareAspectJAutoProxyCreator::class.java)
+            refresh()
+        }
+
+        // When
+        try {
+            val throwError = true
+            context.getBean(SampleBean::class.java).sampleMethod(throwError)
+        } catch (_: Throwable) {
+            // do nothing
+        }
+
+        // Then
+        val expectedLog = listOf(
+            "Before (Around)",
+            "Before",
+            "After Throwing",
+            "After",
+            "After Throwing (Around)",
+            "After (Around)",
+        )
+        assertEquals(expectedLog, SampleAspect.log)
+    }
+
+    @BeforeTest
+    fun setup() {
+        SampleAspect.log = mutableListOf()
+    }
+
+    // ===== 테스트용 클래스 =====
+
+    open class SampleBean {
+        open fun sampleMethod(throwError: Boolean = false) {
+            if (throwError) {
+                throw RuntimeException("Sample exception")
+            }
+        }
+    }
+
+    @Aspect
+    class SampleAspect {
+        companion object {
+            var log: List<String> = mutableListOf()
+        }
+
+        @Pointcut("execution(* spring_context_usage.aop.AdvicesTest.SampleBean.sampleMethod(..))")
+        fun sampleMethod() {
+            // Pointcut for sampleMethod
+        }
+
+        @Before("sampleMethod()")
+        fun beforeAdvice() {
+            log = log + "Before"
+        }
+
+        @AfterReturning("sampleMethod()")
+        fun afterReturningAdvice() {
+            log = log + "After Returning"
+        }
+
+        @AfterThrowing("sampleMethod()", throwing = "ex")
+        fun afterThrowingAdvice(ex: Throwable) {
+            log = log + "After Throwing"
+        }
+
+        @After("sampleMethod()")
+        fun afterAdvice() {
+            log = log + "After"
+        }
+
+        @Around("sampleMethod()")
+        fun aroundAdvice(joinPoint: org.aspectj.lang.ProceedingJoinPoint) {
+            log = log + "Before (Around)"
+            try {
+                joinPoint.proceed()
+            } catch (e: Throwable) {
+                log = log + "After Throwing (Around)"
+            }
+            log = log + "After (Around)"
+        }
+    }
+}
