1bebe7a1666ec73862485d261fc5bb1ac28a9f8e
Galacsh <galacsh.dev@gmail.com>
2025-06-22 14:40:24 +0900

    공유 톰캣 인스턴스 외에 추가적인 인스턴스가 필요하여 초기화 코드를 분리함

--- src/test/kotlin/tomcat_core_usage/SharedTomcat.kt
+++ src/test/kotlin/tomcat_core_usage/SharedTomcat.kt
@@ -6,46 +6,27 @@ import org.apache.catalina.core.StandardContext
 import org.apache.catalina.startup.Tomcat
 import org.apache.tomcat.util.descriptor.web.FilterDef
 import org.apache.tomcat.util.descriptor.web.FilterMap
-import java.io.File
-import java.net.ServerSocket
-import java.util.logging.Level
-import java.util.logging.Logger
-import kotlin.io.path.createTempDirectory
+import support.TomcatSupport
 
 object SharedTomcat {
     private lateinit var instance: Tomcat
-    private lateinit var tempDir: File
-    var port = 0; private set
+    val port
+        get() = instance.connector.port
 
     fun initialize() {
         if (::instance.isInitialized) return
 
-        Logger.getLogger("org.apache.catalina").level = Level.SEVERE
-
-        port = randomPort()
-        tempDir = createTempDirectory("tomcat_test").toFile()
-        val baseDir = tempDir.absolutePath
-
-        instance = Tomcat().apply {
-            setSilent(true)
-            getHost()
-            setPort(port)
-            setBaseDir(baseDir)
-
-            // Tomcat 9 부터는 getConnector() 를 명시적으로 호출해야 커넥터를 생성하고 초기화함
-            getConnector()
-            start()
-        }
+        instance = TomcatSupport.initialize(Tomcat())
+        instance.start()
     }
 
     fun cleanUp() {
-        instance.stop()
-        instance.destroy()
-        if (tempDir.exists()) tempDir.deleteRecursively()
+        TomcatSupport.cleanUp(instance)
     }
 
     fun addServlet(basePath: String, servlet: Servlet) {
-        val docBase = tempDir.resolve(basePath.removePrefix("/"))
+        val docBase = instance.server.catalinaBase
+            .resolve(basePath.removePrefix("/"))
         if (!docBase.exists()) docBase.mkdirs()
 
         val servletName = "${basePath.removePrefix("/")}-${servlet::class.simpleName}"
@@ -78,11 +59,5 @@ object SharedTomcat {
         // 이미 시작된 컨텍스트에 필터를 추가한 경우, 필터를 다시 시작해야 함
         context.filterStop()
         context.filterStart()
-    }
-
-    private fun randomPort(): Int {
-        ServerSocket(0).use { socket ->
-            return socket.localPort
-        }
     }
 }
