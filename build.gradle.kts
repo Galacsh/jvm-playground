aae95d689b5bd593938a69d3725acef165bdaaf5
Galacsh <galacsh.dev@gmail.com>
2025-06-22 14:22:13 +0900

    Web MVC 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // https://docs.spring.io/spring-boot/appendix/dependency-versions/coordinates.html
     implementation("org.springframework:spring-core:6.2.7")
     implementation("org.springframework:spring-context:6.2.7")
+    implementation("org.springframework:spring-webmvc:6.2.7")
     implementation("org.aspectj:aspectjweaver:1.9.24")
     implementation("org.apache.tomcat.embed:tomcat-embed-core:10.1.41")
     testImplementation(kotlin("test"))
